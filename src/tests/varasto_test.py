d9b92fb5e246bb891634dba1ab5c8a2c6122e19f
Pyry Sangi <pyry.sangi@helsinki.fi>
2023-10-31 22:02:42 +0200

    Increase branch coverage of varasto to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,48 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus(self):
+        self.assertAlmostEqual(Varasto(0).tilavuus, 0)
+        self.assertAlmostEqual(Varasto(-1).tilavuus, 0)
+        self.assertAlmostEqual(Varasto(-2000).tilavuus, 0)
+
+    def test_virheellinen_saldo(self):
+        self.assertAlmostEqual(Varasto(10, -1).saldo, 0)
+        self.assertAlmostEqual(Varasto(10, -2000).saldo, 0)
+
+    def test_negatiivinen_lisays(self):
+        vanha_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-2)
+        # Saldon ei pitäisi muuttua.
+        self.assertAlmostEqual(self.varasto.saldo, vanha_saldo)
+        self.varasto.lisaa_varastoon(-4000)
+        self.assertAlmostEqual(self.varasto.saldo, vanha_saldo)
+
+    def test_lisaa_enemman_kuin_mahtuu(self):
+        maara = self.varasto.paljonko_mahtuu() + 1
+        self.varasto.lisaa_varastoon(maara)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_negatiivinen_otto(self):
+        self.varasto.lisaa_varastoon(5)
+        vanha_saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(-1)
+        # Saldon ei pitäisi muuttua.
+        self.assertAlmostEqual(self.varasto.saldo, vanha_saldo)
+        self.varasto.ota_varastosta(-2000)
+        self.assertAlmostEqual(self.varasto.saldo, vanha_saldo)
+
+    def test_ota_enemman_kuin_on(self):
+        self.varasto.ota_varastosta(2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_str(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+        self.varasto.lisaa_varastoon(10)
+        self.assertEqual(str(self.varasto), "saldo = 10, vielä tilaa 0")
+        self.varasto.ota_varastosta(4.5)
+        self.assertEqual(str(self.varasto), "saldo = 5.5, vielä tilaa 4.5")
